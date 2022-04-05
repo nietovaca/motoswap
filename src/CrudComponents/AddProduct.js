6cda6bf5ca77570d08086848220395b0e4e7a027
nietovaca <nietovaca@gmail.com>
2022-04-04 20:37:09 -0600

    cleaner code

--- src/CrudComponents/AddProduct.js
+++ src/CrudComponents/AddProduct.js
@@ -4,8 +4,6 @@ import { Box, Modal, Typography }from '@mui/material';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
   
-
-
 const Add = (props) => {
     let emptyProduct = {
         category: 'New' ||'Used'||'Riding gear'||'Casual wear'||'Luggage',
@@ -24,7 +22,6 @@ const Add = (props) => {
     const handleSubmit = (event) => {
         event.preventDefault()
         props.handleCreate(product)
-     
     }
 
     //Modal Open/Close State
@@ -45,7 +42,6 @@ const modalStyle = {
     boxShadow: 24,
     p: 4,
   };
-
     
     return (
         <>
@@ -108,37 +104,3 @@ const modalStyle = {
 }
 
 export default Add; 
-
-{/* <form onSubmit={handleSubmit}>
-                <label htmlFor='title'>Name:</label>
-                <input type='text' name='title' value={product.title} onChange={handleChange}/>
-                <br />
-                <br />
-                <label>Category:</label>
-                <select value={product.category} htmlFor="category" name='category' onChange={handleChange}>
-                    <option value='New'>New</option>
-                    <option value='Used'>Used</option>
-                    <option value='Riding gear'>Riding gear</option>
-                    <option value='Casual wear'>Casual wear</option>
-                    <option value='Luggage'>Luggage</option>
-                </select>    
-                <br />
-                <br />
-                <label htmlFor='description'>Description:</label>
-                <input type='text' name='description' value={product.description} onChange={handleChange}/>
-                <br />
-                <br />
-                <label htmlFor='imgURL'>Image URL:</label>
-                <input type='text' name='imgURL' value={product.imgURL} onChange={handleChange} />
-                <br />
-                <br />
-                <label htmlFor='price'>Price:</label>
-                <input type='number' name='price' value={product.price} onChange={handleChange} />
-                <br />
-                <br />
-                <label htmlFor='amount'>Quantity in Stock:</label>
-                <input type='number' name='amount' value={product.amount} onChange={handleChange}/>
-                <br />
-                <br />
-                <input type='submit' />
-            </form> */}
